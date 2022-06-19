b79d100eafce858dacbf455e828dd4c650304252
alexander.topilskii <alexander.topilskii@gmail.com>
2022-06-19 12:13:46 +0300

    some awesome

--- core/src/com/talex/game/testgame/TestGame.kt
+++
@@ -1,9 +0,0 @@
-package com.talex.game.testgame
-
-import com.badlogic.gdx.Game
-
-class TestGame : Game() {
-    override fun create() {
-        setScreen(TestScreen())
-    }
-}
--- desktop/src/com/talex/game/DesktopLauncher.kt
+++ desktop/src/com/talex/game/DesktopLauncher.kt
@@ -1,21 +1,20 @@
-package com.talex.game;
+package com.talex.game
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.talex.game.freeworld.FreeWorldGame;
+import kotlin.jvm.JvmStatic
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.talex.game.freeworld.FreeWorldGame
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("Free World");
-//		new Lwjgl3Application(new SuperJumper(), config);
-//		new Lwjgl3Application(new Drop(), config);
-//		new Lwjgl3Application(new FreeWorld(), config);
-		new Lwjgl3Application(new FreeWorldGame(), config);
-//		new Lwjgl3Application(new TetrisGame(), config);
-//		new Lwjgl3Application(new MyGdxGame(), config);
-//		new Lwjgl3Application(new KoalioGame(), config);
-	}
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = Lwjgl3ApplicationConfiguration().apply {
+            setForegroundFPS(60)
+            setTitle("Free World")
+            setWindowedMode(1500, 1500)
+        }
+
+        Lwjgl3Application(FreeWorldGame(), config)
+    }
 }
